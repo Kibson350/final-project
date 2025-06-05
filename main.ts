255c4bf653252b997bff99125533523d565322bb
Kibson350 <kiran.gibson@student.csd509j.net>
2025-06-05 14:23:51 -0700

    latest version 6.5.25 2:23

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
 let gridToggle: boolean = true 
 
 tiles.setCurrentTilemap(assets.tilemap`worldSpace`)
+
+sprites
+let thing = sprites.create(assets.image`thebrownfoxjumpsoverthelazydog`, SpriteKind.Player)
